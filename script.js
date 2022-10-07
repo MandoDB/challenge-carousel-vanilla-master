3e29bdb867a8d950df52f224706910a42011eb91
MandoDB <77023822+MandoDB@users.noreply.github.com>
2022-10-07 11:10:58 +0200

    upload

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.body.onload=function() {
-    nbr=5;
+
+     var MyImages = [
+          "imgs/img-1.png",
+          "imgs/img-2.png",
+          "imgs/img-3.png",
+          "imgs/img-4.png",
+          "imgs/img-5.png",
+          "imgs/img-6.png",
+          "imgs/img-7.png",
+          "imgs/img-8.png",
+     ]
+    nbr=MyImages.length;
     p=0;
 
     var container = document.getElementById("container");
@@ -14,4 +25,20 @@ document.body.onload=function() {
         div.style.backgroundImage="url('imgs/img-"+i+".png')";
         container.appendChild(div)
    } 
+
+   d.onclick=function() {
+     if(p>-nbr+1)
+     p--;
+     container.style.transform="translate("+p*800+"px)";
+     container.style.transition = "all 0.5s ease"
+   }
+
+
+   g.onclick=function() {
+     if(p<0)
+     p++;
+     container.style.transform="translate("+p*800+"px)";
+     container.style.transition = "all 0.5s ease"
+   }
 }
+
